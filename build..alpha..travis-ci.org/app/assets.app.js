e069cb79442260bf18743fa7e0d4d60e35e7756d
Travis CI User <travis@example.org>
2018-06-04 08:16:30 +0000

    [ci skip] update gh-pages

--- build..alpha..travis-ci.org/app/assets.app.js
+++ build..alpha..travis-ci.org/app/assets.app.js
@@ -33033,7 +33033,7 @@ local.stateInit({
             "npm_package_homepage": "https://github.com/npmdoc/node-npmdoc-electron",
             "npm_package_name": "npmdoc-electron",
             "npm_package_nameLib": "npmdoc_electron",
-            "npm_package_version": "0.0.2"
+            "npm_package_version": "2018.6.4"
         }
     }
 });
